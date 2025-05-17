006681e4ce41b7555d665ee3cab81238db4ba317
apilacharya <connectwithapil@gmail.com>
2025-05-17 16:02:49 +0545

    fix: title

--- src/components/form/submit-button.tsx
+++ src/components/form/submit-button.tsx
@@ -1,17 +1,19 @@
-'use client'
+"use client";
 import { useFormStatus } from "react-dom";
 import { LucideLoaderCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 type SubmitButtonProps = {
   label: string;
+  icon?: React.ReactNode;
 };
 
-const SubmitButton = ({ label }: SubmitButtonProps) => {
+const SubmitButton = ({ label, icon }: SubmitButtonProps) => {
   const { pending } = useFormStatus();
   return (
     <Button disabled={pending} type="submit">
       {pending && <LucideLoaderCircle className="mr-2 h-4 w-4 animate-spin" />}
+      {icon}
       {label}
     </Button>
   );
