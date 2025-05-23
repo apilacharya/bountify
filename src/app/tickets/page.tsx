9aaf57cd9b85d9bd61a73b63b71fa3098169af78
apilacharya <connectwithapil@gmail.com>
2025-05-23 18:37:50 +0545

    fix: restore to original state

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -6,8 +6,16 @@ import { Spinner } from "@/components/spinner";
 import { CardCompact } from "@/components/card-compact";
 
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
+import { redirect } from "next/navigation";
+import { signInPath } from "@/paths";
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 
 const TicketsPage = async () => {
+  const { user } = await getAuthOrRedirect();
+
+  if (!user) {
+    redirect(signInPath());
+  }
   return (
     <>
       <div className="flex-1 flex flex-col gap-y-8">
