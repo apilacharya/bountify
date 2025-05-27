a3b971388f64de8594379b76bb86fa92fde8367d
apilacharya <connectwithapil@gmail.com>
2025-05-27 06:29:11 +0545

    feat: search bar for filter addition

--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -1,14 +1,18 @@
 import { getTickets } from "../queries/get-tickets";
 import { TicketItem } from "./ticket-item";
+import { SearchInput } from "@/components/search-input";
 
 type TicketListProps = {
   userId?: string;
 };
 
-const TicketList = async ({userId}: TicketListProps) => {
+const TicketList = async ({ userId }: TicketListProps) => {
   const tickets = await getTickets(userId);
   return (
     <div className="flex flex-1 flex-col gap-y-4 items-center animate-fade-in-from-top">
+      <div className="w-full max-w-[420px]">
+        <SearchInput placeholder="Search tickets..." />
+      </div>
       {tickets.map((ticket) => (
         <TicketItem key={ticket.id} ticket={ticket} />
       ))}
